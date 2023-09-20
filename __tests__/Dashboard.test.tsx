aa015b3de8173da6ca5ed8abc4cfe42f8367369b
maxamed1st <maxamed1st@icloud.com>
2023-09-20 23:38:03 +0100

    inject dependency

--- __tests__/Dashboard.test.tsx
+++ __tests__/Dashboard.test.tsx
@@ -1,6 +1,9 @@
 import Dashboard from "@/app/dashboard/page";
 import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
+import Uppy from "@uppy/core";
+import XHR from '@uppy/xhr-upload'
 
 //prevent resize observer undefined error
 const ResizeObserverMock = vi.fn(() => ({
@@ -12,15 +15,24 @@ const ResizeObserverMock = vi.fn(() => ({
 vi.stubGlobal('ResizeObserver', ResizeObserverMock)
 
 describe("Dashboard", () => {
-  it("renders upload form", async () => {
-    render(<Dashboard />);
+  const uppy = new Uppy().use(XHR, {endpoint: "/api/upload"});
+  //allow only one image at a time
+  uppy.setOptions({
+    restrictions: {
+      maxNumberOfFiles: 1,
+      allowedFileTypes: [ "image/*" ],
+    }
+  });
+
+  it("renders upload form", () => {
+    render(<Dashboard uppy={uppy} />);
     const form = document.querySelector('form');
     expect(form).toBeInTheDocument();
   })
 
   it("should contain Uppy instance", () => {
-    const { getByText } = render( <Dashboard />);
-    const uppy = getByText('Uppy');
-    expect(uppy).toBeInTheDocument();
+    const { getByText } = render( <Dashboard uppy={uppy} />);
+    const Uppy = getByText('Uppy')
+    expect(Uppy).toBeInTheDocument();
   })
 })
