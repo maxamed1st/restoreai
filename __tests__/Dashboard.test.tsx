68232f0e6e9342a3916f45ff2a1f077bb0709b1b
maxamed1st <maxamed1st@icloud.com>
2023-09-22 00:06:03 +0100

    test that image should be uploaded

--- __tests__/Dashboard.test.tsx
+++ __tests__/Dashboard.test.tsx
@@ -1,9 +1,9 @@
 import Dashboard from "@/app/dashboard/page";
 import { describe, it, expect, vi } from "vitest";
-import { render, screen } from "@testing-library/react";
-import userEvent from '@testing-library/user-event';
+import { act, render } from "@testing-library/react";
 import Uppy from "@uppy/core";
-import XHR from '@uppy/xhr-upload'
+import path from 'path';
+import fs from 'node:fs';
 
 //prevent resize observer undefined error
 const ResizeObserverMock = vi.fn(() => ({
@@ -14,8 +14,11 @@ const ResizeObserverMock = vi.fn(() => ({
 
 vi.stubGlobal('ResizeObserver', ResizeObserverMock)
 
+//mock a global URL object
+global.URL.createObjectURL = vi.fn();
+
 describe("Dashboard", () => {
-  const uppy = new Uppy().use(XHR, {endpoint: "/api/upload"});
+  const uppy = new Uppy();
   //allow only one image at a time
   uppy.setOptions({
     restrictions: {
@@ -35,4 +38,36 @@ describe("Dashboard", () => {
     const Uppy = getByText('Uppy')
     expect(Uppy).toBeInTheDocument();
   })
+
+  it("should upload image", async () => {
+    //mock event callback
+    const onUpload = vi.fn();
+
+    const imagePath = path.resolve(__dirname, './testImage.jpg');
+    const image = fs.readFileSync(imagePath);
+
+    render( <Dashboard uppy={uppy} /> );
+
+    await act (async () => {
+      //subscribe to upload event
+      uppy.on('upload', onUpload);
+
+      //set metadata
+      uppy.setMeta({
+        enhance: '2x',
+        colorize: 'yes',
+      })
+
+      //upload image
+      uppy.addFile({
+        name: 'testImage.jpg',
+        type: 'image/jpg',
+        data: new File([image], 'testImage', {type: 'image/jpg'}),
+      });
+      await uppy.upload();
+    });
+
+    // Assert upload event to be fired
+    expect(onUpload).toHaveBeenCalled();
+  });
 })
