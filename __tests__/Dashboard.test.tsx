6f23746c122e8dfcee6ab5e95ac2115e1e560c2d
maxamed1st <maxamed1st@icloud.com>
2023-09-26 04:29:29 +0100

    simplify test

--- __tests__/Dashboard.test.tsx
+++ __tests__/Dashboard.test.tsx
@@ -33,51 +33,22 @@ describe("Dashboard", () => {
     const { container } = render(<Dashboard />);
     const uploadInput = container.querySelector('input[type=file]') as HTMLInputElement;
     const submitButton = container.querySelector('button[type=submit]') as HTMLButtonElement;
-    const enhance = container.querySelector('input[value="2"]') as HTMLInputElement;
-    const colorize = container.querySelector('input[value=yes]') as HTMLInputElement;
-
-    //assert the input fields not to be undefined
-    expect(enhance).not.toBeUndefined();
-    expect(colorize).not.toBeUndefined();
-    expect(uploadInput).not.toBeUndefined();
-    expect(submitButton).not.toBeUndefined();
-
-    /* upload image */
 
     //assert upload input and submit button are in the document
     expect(uploadInput).toBeInTheDocument();
     expect(submitButton).toBeInTheDocument();
-    
+
     //assert submit button and upload input are of type submit and file respectively
     expect(uploadInput.type).toBe('file');
     expect(submitButton.type).toBe('submit');
-    
-    //upload the image with fireEvent.change
+
+    /* upload image */
     await waitFor( () => {
       fireEvent.change(uploadInput, { 
         target: { files: [new File(['(⌐□_□)'], 'test.png', { type: 'image/png' })] }
       });
       expect(uploadInput.files).toHaveLength(1);
     });
-
-    /* select enhance factor and colorize */
-
-    //assert enhance and colorize are in the document and of type radio
-    expect(enhance).toBeInTheDocument();
-    expect(colorize).toBeInTheDocument();
-    expect(enhance.type).toBe('radio');
-    expect(colorize.type).toBe('radio');
-
-    //check enhance and colorize
-    await waitFor( () => {
-      enhance.checked = true;
-      colorize.checked = true;
-
-      //assert enhance and colorize are checked
-      expect(enhance.checked).toBe(true);
-      expect(colorize.checked).toBe(true);
-    });
-
 
     /* submit form with image */
     await waitFor( () => {
